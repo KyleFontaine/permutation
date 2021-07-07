4f78b979c086377cd5d0cc1632b0daaf8e1c0a5c
KyleFontaine <kylefontaine79@gmail.com>
2021-07-07 12:56:46 -0600

    added permutation

--- src/permutation.py
+++ src/permutation.py
@@ -1,9 +1,18 @@
 def permutation(n):
     fact = 1
     # compute n!
+    if n < 0:
+        return "undefined"
+  
+    while n >= 1 :
+        fact = fact * n
+        n = n-1
+
     return fact
 
 def n_take_k(n,k):
+    p = permutation(n)/permutation(n-k)
+    return p 
     # find n take k
     return k
 
